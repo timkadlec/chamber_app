8b1e9bc5e2750e9bfb6f1b38c36ea39cb716e0c7
timkadlec <tim@mwq.cz>
2024-10-15 20:09:47 +0200

    StudentStatus added (db model)

--- chamber_app/models/structure.py
+++ chamber_app/models/structure.py
@@ -26,7 +26,7 @@ class StudyProgram(db.Model):
     weight = db.Column(db.Integer)
     
     # Establish relationship to 'Year'
-    years = relationship("Year", backref='study_program', lazy=True)
+    class_years = relationship("ClassYear", backref='study_program', lazy=True)
 
 # Year model
 class ClassYear(db.Model):
@@ -45,6 +45,13 @@ class ClassYear(db.Model):
     def __repr__(self):
         return self.year
 
+class StudentStatus(db.Model):
+    __tablename__ = 'student_status'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    
+    
 # Student model
 class Student(db.Model):
     __tablename__ = 'students'
@@ -54,16 +61,19 @@ class Student(db.Model):
     first_name = db.Column(db.String(256), nullable=False)
     osobni_cislo = db.Column(db.String(256), unique=True, nullable=False)
     guest = db.Column(db.Boolean)
+    active = db.Column(db.Boolean)
     
     instrument_id = db.Column(db.Integer, db.ForeignKey('instrument.id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
     study_program_id = db.Column(db.Integer, db.ForeignKey('study_programs.id'))
     class_year_id = db.Column(db.Integer, db.ForeignKey('class_years.id'))
-
+    student_status_id = db.Column(db.Integer, db.ForeignKey('student_status.id'))
+    
     # Relationships
     instrument = relationship("chamber_app.models.library.Instrument", backref='students', lazy=True)
     teacher = relationship("chamber_app.models.structure.Teacher", backref='students', lazy=True)
     department = relationship("chamber_app.models.structure.Department", backref='students', lazy=True)
     study_program = relationship("StudyProgram", backref='students', lazy=True)
     class_year = relationship("ClassYear", backref='students', lazy=True)
+    student_status = relationship("StudentStatus", backref='students', lazy=True)
